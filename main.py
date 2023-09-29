e29bb7bfe8e2c78f9caf421295c97ebceb727333
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2023-09-29 16:05:47 +0200

    Adds basic agent

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+from langchain.chat_models import ChatOpenAI
 from phospho import Agent, Message
+
+# Load environment variables
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# Define our langchain agent
+chat = ChatOpenAI(model_name="gpt-3.5-turbo",temperature=0.3)
+
+def my_agent(input):
+    messages = [
+        SystemMessage(content="You are a funny and helpful assistant."),
+        HumanMessage(content=input),
+    ]
+    
+    response=chat(messages)
+
+    return response.content
+
+# Package it into a phospho agent
 
 agent = Agent()
 
-@agent.ask()
-def myask(message):
-    print(f"Your message was {message.content}, now I can do stuff in the background")
-
 @agent.chat()
-def mychat(message):
-    return Message(f"Hello {message.content}!")
+def my_chat(message):
+    # Use our agent logic to generate a response
+    response = my_agent(message.content)
+    # Return the response in a Message object
+    return Message(response)
